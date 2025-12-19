114ce2d4f32d5bf6b7dd5ec31004c42a87738baa
Mohit Kumar <119504966+MOHIT29-dev@users.noreply.github.com>
2025-12-19 21:10:30 +0530

    Refactor PlusOne function for clarity and correctness
    Comments has been added

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digit) {
         int n = digit.size();
-        for(int i = n-1; i>=0; i--){
+        for(int i = n-1; i>=0; i--){ //backward traverse since last digit is needed to be incremented
             if(digit[i] < 9){
                 digit[i]++;
                 return digit;
             }
-            digit[i] = 0;
+            digit[i] = 0; // carry forward
         }
         
         digit.insert(digit.begin(),1);
-        return digit;
+        return digit; // all digits are 9 i.e [9,9,9,9,9,9]
     }
     
 };
