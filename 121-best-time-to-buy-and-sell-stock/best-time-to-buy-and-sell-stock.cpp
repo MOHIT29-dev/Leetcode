b681a5229f2a03628d81a642f7a40cba6794c5d2
Mohit Kumar <119504966+MOHIT29-dev@users.noreply.github.com>
2025-12-21 21:32:42 +0530

    Update best-time-to-buy-and-sell-stock.cpp

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -3,9 +3,9 @@ public:
     int maxProfit(vector<int>& prices) {
         int pro = 0;
         int price = INT_MAX;
-        for(int i = 0; i < prices.size(); i++){
-            price = min(price, prices[i]);
-            pro = max(pro, prices[i] - price);
+        for(int i = 0; i < prices.size(); i++){ 
+            price = min(price, prices[i]); 
+            pro = max(pro, prices[i] - price); //updating profit 
         }
         return pro;
     }
