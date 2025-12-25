8b910b182ef3ea1f96520b9aa14fe28fc371cab4
Mohit Kumar <119504966+MOHIT29-dev@users.noreply.github.com>
2025-12-25 23:02:24 +0530

    Rename variable 'left' to 'l' in lengthOfLongestSubstring
    change of variable name

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         vector<int> lastindex(256,-1);
-        int left = 0, maxlen = 0;
+        int l = 0, maxlen = 0;
 
         for (int right = 0 ; right <s.length(); right++){
             if (lastindex[s[right]] >= left){
