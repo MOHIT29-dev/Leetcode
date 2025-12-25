7f56ee1f7dfd5e63a16c6996a2f9ea6462fe8bdb
Mohit Kumar <119504966+MOHIT29-dev@users.noreply.github.com>
2025-12-25 23:00:39 +0530

    Update remove-element.cpp

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -5,7 +5,7 @@ public:
         for (int i=0;i<nums.size();i++){
             if(nums[i] !=val){
                 nums[a]=nums[i];
-                k++;
+                a++;
             }
         }
         return k;
