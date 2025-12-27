bb243c8d3375fde56fd0ae42183ddefc144d1776
Mohit Kumar <119504966+MOHIT29-dev@users.noreply.github.com>
2025-12-27 20:47:39 +0530

    Add comment for array traversal in containsNearbyDuplicate
    Added a comment to clarify the traversal of the array.

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -4,6 +4,7 @@ public:
         // Map to store the last index where each number appeared
         unordered_map<int, int> lastindex;
 
+        // Traverse the array
         for(int i =0 ; i< nums.size(); i++){
             if (lastindex.count(nums[i])){
                 if (i-lastindex[nums[i]] <= k){
